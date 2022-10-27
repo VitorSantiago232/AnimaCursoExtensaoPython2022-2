6f4a1a3f867c760513be4e511b78e9da8a6fcb3e
vitorsantiago12 <114410872+VitorSantiago232@users.noreply.github.com>
2022-10-27 02:09:55 +0000

    Ver notas

---
+++ aula1-2022-10-19main.py
@@ -0,0 +1,21 @@
+# Meu primeiro projeto Python!!!
+# print() = comando de saida
+print("Alo mundo!")
+
+
+
+# Quando quiser guardar uma String! (frase)
+nome = "Vitor Gonçalves"
+# Quando quiser guardar um número inteiro
+idade = 21
+
+#Exibir o meu nome (que está dentro da variavel nome)
+print(nome)
+
+
+#Quando quiser exibir a frase "Minha idade é" completndo com o conteúdo variável idade
+print(f'Minha nome : {nome}')
+print(f'Minha idade : {idade} anos ')
+
+#Quando quiser exibir "Meu nome é ... e tenho ... anos" trocando pelas variáveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-# Meu primeiro projeto Python!!!
-# print() = comando de saida
-print("Alo mundo!")
+#Pede o nome do aulo e sua nota (de 0 a 10) e, se ele tirou nota 10, mostra "{nome}, Você é o bichão, mesmo..."
+nome = input("Digite seu nome: ")
+nota = float(input("Digite sua nota"))
+
+if (nota == 10):
+  print(f"{nome}, é o bichão mesmo...")
+elif (nota >= 6 and < 10):
+  print(f"{nome}, bom trabalho")
+else: # é sempre automaticamente o que as duas condições não tratamento
+  print("Burro, não tirou dez..."
+
