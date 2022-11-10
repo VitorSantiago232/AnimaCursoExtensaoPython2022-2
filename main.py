a6ff9eff5e8298d95dcda3a93bfde79a7033625c
vitorsantiago12 <114410872+VitorSantiago232@users.noreply.github.com>
2022-11-10 00:36:53 +0000

    Criação de funções

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-#Pede o nome do aulo e sua nota (de 0 a 10) e, se ele tirou nota 10, mostra "{nome}, Você é o bichão, mesmo..."
-nome = input("Digite seu nome: ")
-nota = float(input("Digite sua nota"))
+#criação de funções
 
-if (nota == 10):
-  print(f"{nome}, é o bichão mesmo...")
-elif (nota >= 6 and < 10):
-  print(f"{nome}, bom trabalho")
-else: # é sempre automaticamente o que as duas condições não tratamento
-  print("Burro, não tirou dez..."
+preco = 1999.90
+
+#Calcular apenas 5% de imposto, guardar na variavel imposto e exibir na tela
+imposto = preco * 0.05
+print(imposto)
+
+preco2 = 100
+imposto2 = preco2 * 0.05
+print(imposto2)
+
+#Criar uma função calcular_imposto() que calcular um imposto de 5% e retorna a quem pediu...
+#Isso é a declaração da função (Como ela funciona)
+def calcular_imposto(preco_produto):
+  imposto = preco_produto * 0.05
+  return imposto
+
+#Aqui é o uso... aqui é imposto a calcular.. e exibir na tela
+preco = 299
+imposto = calcular_imposto(preco)
+print(imposto)
 
